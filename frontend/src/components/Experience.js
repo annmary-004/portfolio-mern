5f6421028491906ca30e6d69318bc9e475497a23
annmary004 <annmarygeorgy18@gmail.com>
2026-04-30 16:19:27 +0530

    Redesign experience skills section

--- frontend/src/components/Experience.js
+++ frontend/src/components/Experience.js
@@ -2,36 +2,30 @@ import React, { useEffect } from "react";
 import "./Experience.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
-
-import aws from "../assets/aws.png";
-import css from "../assets/css3.png";
-import react from "../assets/react.jpg";
-import html from "../assets/html.png";
-import firebase from "../assets/firebase.png";
-import js from "../assets/javascript.png";
-import node from "../assets/nodejs.png";
-import mongodb from "../assets/mongodb.png";
+import { FaAws, FaCss3, FaHtml5, FaJs, FaNodeJs, FaReact } from "react-icons/fa";
+import {
+  SiFirebase,
+  SiMongodb,
+} from "react-icons/si";
 
 const Experience = () => {
-
   useEffect(() => {
     AOS.init({ duration: 1000, once: true });
   }, []);
 
   const skills = [
-    { img: aws, name: "AWS" },
-    { img: css, name: "CSS3" },
-    { img: react, name: "ReactJS" },
-    { img: html, name: "HTML" },
-    { img: firebase, name: "Firebase" },
-    { img: js, name: "JavaScript" },
-    { img: node, name: "NodeJS" },
-    { img: mongodb, name: "MongoDB" },
+    { Icon: FaAws, name: "AWS", tone: "aws" },
+    { Icon: FaCss3, name: "CSS3", tone: "css" },
+    { Icon: FaReact, name: "ReactJS", tone: "react" },
+    { Icon: FaHtml5, name: "HTML", tone: "html" },
+    { Icon: SiFirebase, name: "Firebase", tone: "firebase" },
+    { Icon: FaJs, name: "JavaScript", tone: "javascript" },
+    { Icon: FaNodeJs, name: "NodeJS", tone: "node" },
+    { Icon: SiMongodb, name: "MongoDB", tone: "mongodb" },
   ];
 
   return (
     <div className="experience" id="experience">
-
       <h2 className="exp-title" data-aos="fade-up">
         EXPERIENCE
       </h2>
@@ -44,12 +38,13 @@ const Experience = () => {
             data-aos="zoom-in"
             data-aos-delay={index * 100}
           >
-            <img src={item.img} alt={item.name} />
+            <div className={`exp-icon exp-icon-${item.tone}`}>
+              <item.Icon />
+            </div>
             <p>{item.name}</p>
           </div>
         ))}
       </div>
-
     </div>
   );
 };
